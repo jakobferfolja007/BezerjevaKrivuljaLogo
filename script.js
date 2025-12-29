ac6ec145ba14f6b424365b4ee44bb5d00ae6ab6d
jakobferfolja007 <jakobferfolja@gmail.com>
2025-12-29 13:20:55 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,34 +3,20 @@ const ctx = canvas.getContext("2d");
 
 ctx.strokeStyle = "#ffcc00";
 ctx.lineWidth = 4;
+ctx.translate(-135, 0); // omogoči premik logota v levo
 ctx.beginPath();
-ctx.translate(-78, -123);
-ctx.scale(0.9, 0.9);
 
-ctx.moveTo(553, 584);
-ctx.bezierCurveTo(561, 333, 468, 98, 440, 561); 
+// Začetek oblike
+ctx.moveTo(464, 402);
+ctx.bezierCurveTo(488, 11, 571, 18, 590, 427);
+ctx.bezierCurveTo(591, 428, 640, 429, 640, 429);
+ctx.bezierCurveTo(630, 35, 490, 12, 444, 201);
+ctx.bezierCurveTo(375, 4, 257, 57, 247, 428);
+ctx.bezierCurveTo(247, 429, 296, 431, 294, 431);
+ctx.bezierCurveTo(306, 12, 406, 15, 415, 404);
+ctx.bezierCurveTo(415, 404, 465, 404, 464, 402);
 
-ctx.moveTo(440, 561); 
-ctx.bezierCurveTo(440, 561, 398, 559, 399, 561); 
-
-ctx.moveTo(397,562);
-ctx.bezierCurveTo( 384, 167, 284, 244, 282, 581);
-
-ctx.moveTo(284, 582);
-ctx.bezierCurveTo(285, 583, 240, 585, 243, 583);
-
-ctx.moveTo(241, 584);
-ctx.bezierCurveTo(248, 187, 390, 229, 418, 378);
-
-ctx.moveTo(418, 378); 
-ctx.bezierCurveTo(457, 222, 588, 193, 598, 588);
-
-
-ctx.moveTo(555, 582); 
-ctx.bezierCurveTo(555, 582, 594, 583, 595, 583);
-
-
-ctx.stroke();
+// Zapremo pot, da bo ena zapolnjena oblika
 ctx.closePath();
 ctx.fillStyle = "#ffcc00";
 ctx.fill();
