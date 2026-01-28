4e3498144bb11195cd31f7b22197956bb5fdccef
jakobferfolja007 <jakobferfolja@gmail.com>
2026-01-28 20:07:54 +0100

    Add files via upload

--- JS/menjavaSlik.js
+++ JS/menjavaSlik.js
@@ -1,3 +1,30 @@
+const heroSlides = [
+    { image: 'images/akcija3.jpg', tag: 'Zimska ponudba', title: 'Legenda je<br>nazaj' },
+    { image: 'images/akcija2.jpg', tag: 'Pomladna akcija', title: 'Novi McCrispy<br>mango curry' },
+    { image: 'images/akcija.jpg', tag: 'Vroča ponudba', title: 'Okusi<br>vročino' }
+];
+
+let slideIndex = 0;
+
+const heroImg = document.getElementById('hero-image');
+const heroTag = document.getElementById('hero-tag');
+const heroTitle = document.getElementById('hero-title');
+
+setInterval(() => {
+    slideIndex = (slideIndex + 1) % heroSlides.length;
+
+    // posodobi sliko
+    heroImg.src = heroSlides[slideIndex].image;
+
+    // posodobi tag
+    heroTag.textContent = heroSlides[slideIndex].tag;
+
+    // posodobi naslov z <br> za dve vrstici
+    heroTitle.innerHTML = heroSlides[slideIndex].title;
+}, 4000);
+
+
+
 
 
 
